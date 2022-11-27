3a5c587987afed758442f98fb13c739176a3c9f4
Jacob Tracey <42984369+Jacob-Tr@users.noreply.github.com>
2022-11-27 07:25:53 -0400

    Add files via upload

---
+++ includes/Calculate.h
@@ -0,0 +1,19 @@
+#ifndef CALCULATE_H
+#define CALCULATE_H
+
+#ifndef UTILS_H
+    #include "includes/Utils.h"
+#endif
+
+#ifdef _cplusplus
+extern "C"
+{
+#endif
+
+
+
+#ifdef _cplusplus
+}
+#endif
+
+#endif
